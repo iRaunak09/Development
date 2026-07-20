c7b801fd3294eeca7c273d9cccec8cd6dd3db54a
iRaunak09 <raunakbothra02@gmail.com>
2026-07-20 18:39:32 +0530

    Added more practice work

---
+++ Practice_AC/Practice_Js/Part2AssignmentQ.js
@@ -0,0 +1,27 @@
+// // Largest of 3 numbers
+// let a = 19;
+// let b = 18;
+// let c = 19;
+
+// if(a >= b && a >= c){
+//     console.log(`${a} is the largest of three values`);
+// }else if(a <= b && b >= c){
+//     console.log(`${b} is the largest of three values`);
+// }else if(a <= c && b <= c){
+//     console.log(`${c} is the largest of three values`);
+// }
+
+// // To check the last digit
+// let d = 10;
+// let e = 200;
+
+// if ((d % 10) === (e % 10)){
+//     console.log("Same");
+// }else {
+//     console.log("not");
+// }
+
+let a = "Name";
+console.log(a);
+a = "Raunak";
+console.log(a);
---
+++ Practice_AC/Practice_Js/PracticeQ1.js
@@ -0,0 +1,7 @@
+let str = "abcd";
+
+if(str[0] === 'a' && (str.length > 3)){
+    console.log("Good String");
+} else {
+    console.log("Needs Improvement");
+}
