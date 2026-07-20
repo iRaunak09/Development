e8530d6dbb9b1abba20cdd79c578ffd3ee06cbe2
iRaunak09 <raunakbothra02@gmail.com>
2026-07-20 12:03:09 +0530

    Practice Q

---
+++ Practice_AC/Practice_Js/JsPractice.js
@@ -0,0 +1,19 @@
+// let a = 10;
+// let b = 9;
+// let output = `The price is ${a + b} Rupees`;
+// console.log("The price is " + (a + b) + " Rupees");
+// console.log(output);
+
+let color = "Red";
+if(color === "Red"){
+    console.log("Stop!!");
+}
+if(color === "Yellow"){
+    console.log("Wait!!");
+}
+if(color === "Green"){
+    console.log("Go!!");
+}
+
+
+
