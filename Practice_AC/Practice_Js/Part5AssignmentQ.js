c94bc0b4f8318e39058aa8f8f26b8c70f7d0c1a1
iRaunak09 <raunakbothra02@gmail.com>
2026-07-24 00:05:49 +0530

    More Practice Stuff

---
+++ Practice_AC/Practice_Js/Part5AssignmentQ.js
@@ -0,0 +1,21 @@
+// Q1
+let ran = Math.floor(Math.random() * 6) + 1;
+console.log(ran);
+
+// Q2
+const carDetails = {
+    companyName : "Hyundai",
+    model : "i20",
+    color : "white"
+};
+console.log(carDetails.companyName);
+
+// Q3
+const person = {
+    name : "John",
+    age : 19,
+    city : "California"
+};
+person.city = "New York"
+person.country = "USA";
+console.log(person);
