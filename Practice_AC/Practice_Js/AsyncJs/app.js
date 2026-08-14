cf91b63cd8de965079be32f51057c2a2d11d8e37
iRaunak09 <raunakbothra02@gmail.com>
2026-08-14 23:25:36 +0530

    Js async nature practice

---
+++ Practice_AC/Practice_Js/AsyncJs/app.js
@@ -0,0 +1,40 @@
+// let h1 = document.querySelector("h1");
+
+// function changeColor(color,delay,nextColorChange){
+//     setTimeout(() => {
+//         h1.style.color = color;
+//         nextColorChange();
+//     },delay);
+// };
+// // setTimeout(() => {
+// //     changeColor("red");
+// //     // h1.style.color = "red";
+// // }, 1000);
+
+// // setTimeout(() => {
+// //     // h1.style.color = "yellow";
+// //     changeColor("green");
+// // }, 2000);
+
+// // setTimeout(() => {
+// //     changeColor("yellow");
+// //     // h1.style.color = "green";
+// // }, 3000);
+
+// // setTimeout(changeColor, 1000);
+// // setTimeout(changeColor, 2000);
+// // setTimeout(changeColor, 3000);
+
+// // changeColor('red', 1000);
+
+// changeColor("red", 1000, () => {
+//     changeColor("orange", 1000, () => {
+//         changeColor("green", 1000);
+//     });
+// });
+
+
+
+
+
+
