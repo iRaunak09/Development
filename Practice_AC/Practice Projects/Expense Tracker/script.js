f11fc7d69ca42b18ef7c75b228866d0957c775bc
iRaunak09 <raunakbothra02@gmail.com>
2026-08-29 01:37:31 +0530

    edit functionality done

--- Practice_AC/Practice Projects/Expense Tracker/script.js
+++ Practice_AC/Practice Projects/Expense Tracker/script.js
@@ -4,17 +4,34 @@ let amt = document.querySelector("#amt");
 let date = document.querySelector("#date");
 let tbody = document.querySelector("tbody");
 let catergory = document.querySelector("#category");
-// let del = document.querySelector(".delete");
-// let edit = document.querySelector(".edit");
-
+let state = "add";
+let currentEditingRow = null;
 
 add.addEventListener("click", (event) => {
-    event.preventDefault();
-    createTableRow();
-    // expName.value = "";
-    // amt.value = "";
-    // date.value == "";
-    // catergory.value == "";
+    if(state === "add"){
+        event.preventDefault();
+        createTableRow();
+    }else if(state === "edit"){
+        event.preventDefault();
+        currentEditingRow.innerHTML = `<td>${expName.value}</td>
+                                    <td>$${amt.value}</td>
+                                    <td>${catergory.value}</td>
+                                    <td>${date.value}</td>
+                                    <td>
+                                        <button class="edit">Edit</button>
+                                        <button class="delete">Delete</button>
+                                    </td>`;
+
+        state = "add";
+        add.innerText = "Add Expense";
+        addListenerDelete();
+        addListenerEdit();
+        expName.value = "";
+        amt.value = "";
+        date.value = "";
+        catergory.value = "";
+    }
+    
 });
 
 function createTableRow(){
@@ -35,33 +52,8 @@ function createTableRow(){
             tbody.insertAdjacentHTML('beforeend', innerHtml);
 
 
-            let del = document.querySelectorAll(".delete");
-            for(btn of del){
-                btn.addEventListener("click", (event) => {
-                    console.log("Delete button clicked"); 
-                    console.log(event.target.parentElement.parentElement);
-                    event.target.parentElement.parentElement.remove();
-                });
-            }
-
-            let edit = document.querySelectorAll(".edit");
-            for(ed of edit){
-                ed.addEventListener("click",(event) => {
-                    console.log((event.target.parentElement.parentElement.children[0]));
-
-                    let tr = event.target.parentElement.parentElement.children;
-                    expName.value = tr[0].innerHTML;
-                    amt.value = tr[1].innerHTML.replace("$","");
-                    catergory.value = tr[2].innerHTML;
-                    date.value = tr[3].innerHTML;
-                    console.log("Edit button was pressed");
-
-                    add.innerText = "Update";
-                    add.addEventListener("click", (event) => {
-                        
-                    });
-                });
-            }
+            addListenerDelete();
+            addListenerEdit();
 
             expName.value = '';
             amt.value = '';
@@ -70,4 +62,37 @@ function createTableRow(){
 
     }
     
+}
+
+function addListenerEdit(){
+    let edit = document.querySelectorAll(".edit");
+            for(ed of edit){
+                ed.addEventListener("click",(event) => {
+                    // console.log((event.target.parentElement.parentElement.children[0]));
+
+                    let tr = event.target.parentElement.parentElement.children;
+                    let tr1 = event.target.parentElement.parentElement;
+                    console.log(tr1.innerHTML);
+                    expName.value = tr[0].innerHTML;
+                    amt.value = tr[1].innerHTML.replace("$","");
+                    catergory.value = tr[2].innerHTML;
+                    date.value = tr[3].innerHTML;
+                    console.log("Edit button was pressed");
+
+                    currentEditingRow = event.target.parentElement.parentElement;
+                    add.innerText = "Update";
+                    state = "edit";
+                });
+            }
+}
+
+function addListenerDelete(){
+    let del = document.querySelectorAll(".delete");
+            for(btn of del){
+                btn.addEventListener("click", (event) => {
+                    console.log("Delete button clicked"); 
+                    console.log(event.target.parentElement.parentElement);
+                    event.target.parentElement.parentElement.remove();
+                });
+            }
 }
